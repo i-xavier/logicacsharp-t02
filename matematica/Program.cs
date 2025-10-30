ffb0b577fbc542052b63dff2e1fbfdfa1cea089e
IGOR XAVIER DOS SANTOS <igor.xsantos1@senacsp.edu.br>
2025-10-30 17:04:26 -0300

    aula 30-10

---
+++ matematica/Program.cs
@@ -0,0 +1,51 @@
+﻿// Declarando constantes
+
+const int valor1 = 10;
+const int valor2 = 5;
+const float valor3 = 3.0F;
+
+// Soma
+float total = valor1 + valor2;
+
+Console.WriteLine($"{valor1} + {valor2} = {total}");
+
+// Subtração
+total = valor1 - valor2;
+
+Console.WriteLine($"{valor1} - {valor2} = {total}");
+
+// Multiplicação
+total = valor1 * valor2;
+
+Console.WriteLine($"{valor1} * {valor2} = {total}");
+
+// Divisão
+total = valor1 / valor3;
+
+Console.WriteLine($"{valor1} / {valor3} = {total}");
+
+// Resto da divisão
+total = valor1 % valor2;
+
+Console.WriteLine($"{valor1} % {valor2} = {total}");
+
+// Potência
+double potencia = Math.Pow(valor1, valor2);
+
+Console.WriteLine($"{valor1} ^ {valor2} = {potencia}");
+
+// Exemplo 1: Sem parênteses
+int resultado1 = 3 + 5 * 2; //Multiplicação tem prioridade
+Console.WriteLine($"Resultado 1 (sem parenteses): 3 + 5 * 2 = {resultado1}");
+
+// Exemplo 1: Com parênteses
+resultado1 = (3 + 5) * 2; // Parenteses alteram a ordem
+Console.WriteLine($"Resultado 2 (com parenteses): 3 + 5 * 2 = {resultado1}");
+
+// Exemplo 2: Sem parênteses
+resultado1 = 8 / 4 + 3; // Divisão tem prioridade
+Console.WriteLine($"Resultado 3 (sem parenteses): 3 + 5 * 2 = {resultado1}");
+
+// Exemplo 2: Com parênteses
+double resultado2 = 8.0 / (4 + 3) ; //Parenteses alteram a ordem
+Console.WriteLine($"Resultado 4 (com parenteses): 3 + 5 * 2 = {resultado2}");
---
+++ variaveisEconstantes/Program.cs
@@ -0,0 +1,41 @@
+﻿//Variáveis
+char letra = 'C';
+string nome = "João";
+float temperatura = 25.7F;
+double salario = 12578.89;
+bool estaChovendo = true;
+var list = new List<string> { "Eu", "Me", "Remexo", "Muito" };
+
+//Alterando as variáveis
+letra = 'X';
+nome = "Maria";
+temperatura = 39.7F;
+salario = 1.99;
+estaChovendo = false;
+
+//Constantes
+const char simbolo = '$';
+const string saudacao = "Receba na caixa dos seios";
+const float pi = 3.14159F;
+const double altura = 1.78;
+const bool ehVerdade = true;
+const int numero = 14;
+
+
+//Exibindo os valores os valores originais por interpolação
+Console.WriteLine("Valores originais");
+Console.WriteLine($"Letra: {letra}");
+Console.WriteLine($"Nome: {nome}");
+Console.WriteLine($"Temperatura: {temperatura}");
+Console.WriteLine($"Salario: {salario}");
+Console.WriteLine($"Está chovendo? {estaChovendo}");
+
+//Exibindo os valores por concatenação 
+
+Console.WriteLine("\nConstantes");
+Console.WriteLine("Simbolo: " + simbolo);
+Console.WriteLine("Saudação: " + saudacao);
+Console.WriteLine("Valor de pi: " + pi);
+Console.WriteLine("Altura: " + altura);
+Console.WriteLine("É verdade? " + ehVerdade);
+Console.WriteLine("Numero? " + numero);
