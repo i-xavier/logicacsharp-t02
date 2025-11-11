2f915188bec2b75c6c5537ab65e734566f31998e
IGOR XAVIER DOS SANTOS <igor.xsantos1@senacsp.edu.br>
2025-11-11 15:37:19 -0300

    exclusão de diretorio

---
+++ condicionais/Program.cs
@@ -0,0 +1,34 @@
+﻿//Condicionail IF
+string nome = "";
+/*
+if (nome == "Ana")
+{
+    Console.WriteLine("Olá, Ana!");
+}
+
+if(string.IsNullOrEmpty(nome)) {
+    Console.WriteLine("A string está vazia.");
+}*/
+
+// Condicional IF/ELESE
+int idade = 16;
+
+if (idade>=18)
+{
+    Console.WriteLine("Você é maior de idade.\n");
+}
+else
+{
+    Console.WriteLine("Você é menor de idade.\n");
+}
+
+int numero = 7;
+
+if (numero%2 == 0)
+{
+    Console.WriteLine("O número é par.\n");
+}
+else
+{
+    Console.WriteLine("O numero é ímpar.\n");
+}
