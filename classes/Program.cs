f2db6cd470c6a6dff8b70293e66c429d89691e1a
IGOR XAVIER DOS SANTOS <igor.xsantos1@senacsp.edu.br>
2025-12-02 16:56:03 -0300

    aula 02/12

---
+++ classes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica13
+{
+    //Definição da classe Carro
+
+    internal class Carro
+    {
+        //Propriedade pública: Pode ser acessada e modificada fora da classe
+        public string Marca { get; set; } // Publica a propriedade Marca
+
+        //Propriedade privada: Não pode ser acessada ou modificada fora da classe
+        private string Modelo { get; set; } // Privada, acessível apenas dentro da classe
+
+        //Propriedade privada com um campo privado e um método getter e setter personalizados
+        private int _ano { get; set; } // Campo privado
+
+        public int Ano
+        {
+            get { return _ano; } //Getter, para acessar o valor do campo privado
+            set 
+            {
+                if (value > 1900 && value <= DateTime.Now.Year) //Verificação para garantir um valor válido
+                {
+                    _ano = value;
+                }
+                else {
+                    Console.WriteLine("Ano inválido. ");
+                }
+                        
+            }
+        }
+    }
+}
